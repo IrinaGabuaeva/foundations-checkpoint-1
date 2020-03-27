fbaf9c9de4b2820a91118c965dcbfe54082883c0
Irina Gabuaeva <irinagabuaeva@gmail.com>
2020-03-27 15:26:23 -0400

    completed 1st exercise

--- 00_find_obj_properties/solution_find_obj_properties.js
+++ 00_find_obj_properties/solution_find_obj_properties.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+const findObjPropsHasOwn = obj => {
+  let array = [];  
+    for(let key in obj){
+if(obj.hasOwnProperty([key])){
+    array.push(key)
+  }
+ } return array.join(", ")
+}
+
+const findObjKeys = obj => {
+ let array = Object.keys(obj);
+ return array.join(", ")
+}
