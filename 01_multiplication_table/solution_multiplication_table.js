dce609b63346afaa7fb3be65d53a9ef80144bea7
Irina Gabuaeva <irinagabuaeva@gmail.com>
2020-03-27 16:18:20 -0400

    completed 2nd exercise

--- 01_multiplication_table/solution_multiplication_table.js
+++ 01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+const multiplicationTable = (rows,columns) => {
+   let array = [];
+    
+    for (let i = 1; i <= rows; i++){
+       let currentArr = []; 
+         for(let j = 1; j <= columns; j++){
+         currentArr.push(i*j);
+         } 
+         array.push(currentArr);
+    } 
+    console.log(array)
+    return array;
+}
